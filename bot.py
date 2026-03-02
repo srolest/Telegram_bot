e4c9c68e5a2f507af7b7f435d36bb35fc7f5ed07
srolest <serafinroldanesteban@gmail.com>
2026-03-02 11:24:05 +0100

    Subida de archivos finales con alguna modificación en el bot y añadiendo el gitignore para no incluir el token

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import random  # Librería para la función del dado
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -7,7 +8,26 @@ bot = telebot.TeleBot(BOT_TOKEN)
 # Registrar manejadores de mensajes
 @bot.message_handler(commands=['start', 'hola'])
 def enviar_bienvenida(mensaje):
-    bot.reply_to(mensaje, "Hola, ¿cómo estás?")
+    bot.reply_to(mensaje, "Hola, ¿cómo estás? Soy SRbot ¿En que puedo ayudarte?")
+
+# Comando /info
+@bot.message_handler(commands=['info'])
+def enviar_info(mensaje):
+    info_text = (
+        "🤖 **Ficha Técnica de SRbot**\n\n"
+        "🔹 **Desarrollador:** Serafín Roldán Esteban\n"
+        "🔹 **Entorno:** Contenedor Docker (Python 3)\n"
+        "🔹 **Librería:** pyTelegramBotAPI\n"
+        "🔹 **Estado:** Operativo y aprendiendo."
+    )
+    bot.reply_to(mensaje, info_text, parse_mode='Markdown')
+
+# Comando /dado 
+@bot.message_handler(commands=['dado'])
+def lanzar_dado(mensaje):
+    numero = random.randint(1, 6)
+    bot.reply_to(mensaje, f"🎲 Has lanzado el dado y ha salido un: {numero}")
+
 
 @bot.message_handler(func=lambda msg: True)
 def enviar_echo(mensaje):
